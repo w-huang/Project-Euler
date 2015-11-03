594e22165dd37222d3dadcfaa49df776d3e36a4c
Wilson Huang <wilsonwei.huang@mail.utoronto.ca>
2015-11-03 11:09:02 -0500

    q26

--- q26.java
+++ q26.java
@@ -5,20 +5,35 @@ import java.util.*;
 import helpers.helpers;
 public class q26 {
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] flag = new int[1000];
-		for(int i = 1; i < 1001; ++i){
-			if(repeats(i)) flag[i-1] = 1;
+		int max = 1;
+		int value = 1;
+		for(int i = 2; i < 1001; ++i){
+			int m = i;
+			while(m%2 == 0){
+				m /= 2;
+			}
+			while(m%5 == 0){
+				m /= 5;
+			}
+			int k = lengthRepeat(m);
+			if( k > max){
+				max = k;
+				value = i;
+			}
 		}
-		
+		System.out.println(value);
 	}
 
-	public static boolean repeats(int x){
-		ArrayList<int[]> primes = helpers.primeFactors((long) x);
-		for(int i = 0; i < primes.size(); ++i){
-			if(primes.get(i)[0] != 2 || primes.get(i)[0] != 5) return true;
+	public static int lengthRepeat(int mantissa){
+		BigDecimal i = new BigDecimal(9);
+		while(i.remainder(BigDecimal.valueOf(mantissa)) != BigDecimal.ZERO){
+			i = i.multiply(BigDecimal.valueOf(10)); 
+			i = i.add(BigDecimal.valueOf(9));
 		}
-		return false;
+		return i.toString().length();
 	}
+	
 }
